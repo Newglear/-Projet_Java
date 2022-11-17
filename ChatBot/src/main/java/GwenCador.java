bb5c8b84c09e0191a28022d333826fba514264a4
Newg <g_robert@insa-toulouse.fr>
2022-11-17 11:33:44 +0100

    Network commit 1 test broadcast

--- ChatBot/src/main/java/GwenCador.java
+++
@@ -1,5 +0,0 @@
-public class GwenCador {
-    public static void main(String[] args) {
-        System.out.println("Gwen"); //Ceci est un commentaire
-    }
-}
---
+++ ChatBot/src/main/java/NetworkSender.java
@@ -0,0 +1,35 @@
+import java.net.*;
+
+public class NetworkSender {
+
+    public static void main(String[] args) throws SocketException, UnknownHostException {
+        DatagramSocket dgramSocket = null;
+        try {
+            dgramSocket = new DatagramSocket();
+        } catch (Exception e) {
+            System.out.println("Connection Denied !");
+        }
+        dgramSocket.setBroadcast(true);
+
+        String message = new String("Sending some shit");
+        //byte[] addr= {10,32,46, (byte) 149};
+        byte[] addr = {(byte) 255, (byte) 255, (byte) 255, (byte) 255};
+        DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(),InetAddress.getByAddress(addr), 1234);
+        try {
+            dgramSocket.send(outPacket);
+        } catch (Exception e){
+            System.out.println("Je sais pas quel pb");
+        }
+        byte[] buffer = new byte[256];
+        DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+
+        try {
+            dgramSocket.receive(inPacket);
+        } catch (Exception e){
+            System.out.println("Une autre erreur inconnue");
+        }
+        String response = new String(inPacket.getData(), 0, inPacket.getLength());
+        System.out.println(response);
+        dgramSocket.close();
+    }
+}
