d0f1c84f0562ff88126e27ffa9613b24185c82b2
EvanRz <evan.roziere@hotmail.com>
2022-12-12 16:20:43 +0100

    Finished ThreadManager

--- ChatBot/src/main/java/ThreadManager.java
+++ ChatBot/src/main/java/ThreadManager.java
@@ -3,10 +3,16 @@ import java.util.ArrayList;
 
 public class ThreadManager {
     public static ArrayList<ReceiverThread> receiverThreadList = new ArrayList<>();
+    public static ArrayList<SenderThread> senderThreadList = new ArrayList<>();
 
     public static void createReceiverThread(ServerSocket socket) {
         receiverThreadList.add(new ReceiverThread(socket));
         receiverThreadList.get(receiverThreadList.size()-1).start();
+    }
+
+    public static void createSenderThread(InetAddress IPaddress, int publicPort, String message) {
+        senderThreadList.add(new SenderThread(IPaddress,publicPort,message));
+        senderThreadList.get(senderThreadList.size()-1).start();
     }
 
 }
