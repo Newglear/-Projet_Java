0d46eafbd7c95986d0444feb3e74530f7fa9c4d0
g_robert <g_robert@etud.insa-toulouse.fr>
2022-12-14 09:35:31 +0100

    System Components et DB prete

--- ChatBot/src/main/java/SystemComponents.java
+++ ChatBot/src/main/java/SystemComponents.java
@@ -1,13 +1,20 @@
-public class SystemComponents {
-    public DatabaseManager db;
+import java.io.IOException;
 
-    public static ThreadManager ThreadManager;
+public class SystemComponents {
+    public static DatabaseManager db;
+
+    public static ThreadManager Threads;
 
     public static String currentNickname;
     public static String currentIp;
 
     public SystemComponents(){
         db = new DatabaseManager();
-
+        try{
+            Threads = new ThreadManager();
+            ThreadManager.StartConnection();
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
